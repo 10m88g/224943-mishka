5db6e8a5bde7363517594c20a1c2b2f55b112bee
Lubov Andreeva <bananamilk@ya.ru>
2017-03-27 23:57:23 +0300

    Добавление некоторых элементов, js

--- js/script-modal.js
+++ js/script-modal.js
@@ -1,3 +1,24 @@
+var link = document.querySelector(".item__button-overlay");
+var overlay = document.querySelector(".overlay");
+var close = overlay.querySelector(".modal__close");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  overlay.classList.add("overlay__show");
+});
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  overlay.classList.remove("overlay__show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (overlay.classList.contains("overlay__show")) {
+    overlay.classList.remove("overlay__show");
+    }
+  }
+});
+
 var link = document.querySelector(".item__button-order");
 var popup = document.querySelector(".modal");
 var close = popup.querySelector(".modal__close");
@@ -10,6 +31,7 @@ close.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.remove("modal__show");
 });
+
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
     if (popup.classList.contains("modal__show")) {
