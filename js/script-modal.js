974d362d89817a75128288df7362e72bda051ba8
Lubov Andreeva <bananamilk@ya.ru>
2017-04-02 01:36:48 +0300

    Критерии

--- js/script-modal.js
+++ js/script-modal.js
@@ -1,42 +1,33 @@
-
-var layer = document.querySelector(".item__button-overlay");
+var link = document.querySelector(".item__button-order");
+var popup = document.querySelector(".modal");
 var overlay = document.querySelector(".overlay");
-var closing = overlay.querySelector(".modal__closing");
 
-layer.addEventListener("click", function(event) {
-  event.preventDefault();
-  overlay.classList.add("overlay__show");
+for(var i=0;i<Link.length;i++) {
+  Link[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("modal__show");
+    overlay.classList.add("overlay__show");
+  });
+
+  overlay.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal__show");
+    overlay.classList.remove("overlay__show");
 });
-close.addEventListener("click", function(event) {
-  event.preventDefault();
-  overlay.classList.remove("overlay__show");
+}
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("modal__show")) {
+    popup.classList.remove("modal__show");
+    }
+  }
 });
 
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
     if (overlay.classList.contains("overlay__show")) {
     overlay.classList.remove("overlay__show");
-    }
-  }
-});
-
-var link = document.querySelector(".item__button-order");
-var popup = document.querySelector(".modal");
-var close = popup.querySelector(".modal__close");
-
-link.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup.classList.add("modal__show");
-});
-close.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup.classList.remove("modal__show");
-});
-
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (popup.classList.contains("modal__show")) {
-    popup.classList.remove("modal__show");
     }
   }
 });
