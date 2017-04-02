83cb98916b76ab3d3d15b56c09e79ffe7b73a5be
Lubov Andreeva <bananamilk@ya.ru>
2017-04-02 19:49:15 +0300

    Исправила замечания

--- js/script-modal.js
+++ js/script-modal.js
@@ -1,33 +1,29 @@
 var link = document.querySelector(".item__button-order");
+var linkbasket = document.querySelectorAll(".catalog__item-order");
+
 var popup = document.querySelector(".modal");
-var overlay = document.querySelector(".overlay");
+var overlay = document.querySelector(".modal__overlay");
 
-for(var i=0;i<Link.length;i++) {
-  Link[i].addEventListener("click", function(event) {
+  if (link) {
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+      popup.classList.add("modal__show");
+    });
+  };
+
+  if (overlay) {
+    overlay.addEventListener("click", function(event) {
+      event.preventDefault();
+      popup.classList.remove("modal__show");
+    });
+  };
+
+for (var i = 0; i < linkbasket.length; i++) {
+  linkbasket[i].addEventListener("click", function(event) {
     event.preventDefault();
-    popup.classList.add("modal__show");
-    overlay.classList.add("overlay__show");
+      if (popup) {
+        popup.classList.add("modal__show");
+      }
   });
+};
 
-  overlay.addEventListener("click", function(event) {
-    event.preventDefault();
-    popup.classList.remove("modal__show");
-    overlay.classList.remove("overlay__show");
-});
-}
-
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (popup.classList.contains("modal__show")) {
-    popup.classList.remove("modal__show");
-    }
-  }
-});
-
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (overlay.classList.contains("overlay__show")) {
-    overlay.classList.remove("overlay__show");
-    }
-  }
-});
